d9ae6c3f1825bd2037ab1dfe2125c8b02469b55d
irais-valenzuela <iraisvq8@gmail.com>
2024-02-23 12:01:02 -0800

    changed if else chain to a ternary

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 document.getElementById("toggle").addEventListener('change', () => {
     const htmlTag = document.documentElement
-    const dataThemeValue = htmlTag.getAttribute("data-theme")
+    const currentDataThemeValue = htmlTag.getAttribute("data-theme")
 
-    if (dataThemeValue === "dark") htmlTag.setAttribute("data-theme", "light")
-    else if (dataThemeValue === "light") htmlTag.setAttribute("data-theme", "dark")
+    currentDataThemeValue === "dark" ?
+        htmlTag.setAttribute("data-theme", "light") :
+        htmlTag.setAttribute("data-theme", "dark")
 })
